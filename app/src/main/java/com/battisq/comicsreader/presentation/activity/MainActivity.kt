2199f7af7e32e18fc2b9472f29d50373db76a5f7
battisq <airget2000@gmail.com>
2022-03-27 20:31:35 +0700

    feat: Create project

---
+++ app/src/main/java/com/battisq/comicsreader/presentation/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.battisq.comicsreader.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.battisq.comicsreader.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
